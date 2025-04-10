e1c9f74d1dba1d921c13566bf336427365f4cf81
Murtaza Amjad <105024115+GokuSSGodd@users.noreply.github.com>
2025-04-10 15:54:54 -0400

    Part 3 of Software
    Adding all of the files and then will delete the old ones

--- Sniff.hpp
+++ Sniff.hpp
@@ -4,18 +4,18 @@
 #include "FileID.hpp"
 #include "Printer.hpp"
 
-class Sniff
-{
+class Sniff{
 private:
   Params param;
-  string dirName;  // name of the file
-  string pathName; // name of the path
+  string dirName; //name of the file
+  string pathName; //name of the path
   vector<string> unpackWords;
   vector<FileID> susFiles;
   struct dirent *entry;
-
 public:
   Sniff(Params param);
   void oneDir();
   FileID oneFile();
+  void run(string&);
+  void travel(string&);
 };
--- tools.cpp
+++ tools.cpp
@@ -9,46 +9,43 @@
 // I/O Manipulators.
 // -------------------------------------------------------------------
 // Used to discard the remainder of the current input line
-istream &
-cleanline(istream &is)
-{
-    return is.ignore(numeric_limits<streamsize>::max(), '\n');
+istream&
+cleanline( istream& is ) {
+	return is.ignore(numeric_limits<streamsize>::max(), '\n');
 }
 
 // -------------------------------------------------------------------
 // Used to flush an input stream buffer
 //    as in myin >> x >> flush;  or cin >> flush;
-istream &
-flush(istream &is)
-{
-    return is.seekg(0, ios::end);
+istream&
+flush( istream& is ) {
+    return is.seekg( 0, ios::end );
 }
 
 // -------------------------------------------------------------------
 // Reset formatting style from fixed or scientific back to default.
 // Use: cout <<fixed <<x <<general <<y;
-ostream &general(ostream &os)
-{
-    os.unsetf(ios::floatfield);
-    return os;
+ostream& general( ostream& os ){
+	os.unsetf( ios::floatfield );
+	return os;
 }
 
 // -------------------------------------------------------------------
 // Routine output labeling. ------------------------------------------
 // -------------------------------------------------------------------
-void fbanner(ostream &fout)
-{
+void
+fbanner( ostream& fout ) {
     string date, time;
     when(date, time);
     fout << "\n----------------------------------------------------------------\n"
          << "\t" << NAME
-         << "\n\t" << CLASS << "\n\t" << date << "\t" << time << '\n'
+         << "\n\t" << CLASS << "\n\t" <<  date << "\t" << time << '\n'
          << "----------------------------------------------------------------\n";
 }
 
-void banner() { fbanner(cout); }
+void banner() { fbanner( cout ); }
 
-void bye() { Printer::write("\nNormal termination.\n", cerr); }
+void bye() { Printer::write("\nNormal termination.\n",cerr) ; }
 
 // -------------------------------------------------------------------
 //  Error handling and error recovery functions.
@@ -56,8 +53,8 @@ void bye() { Printer::write("\nNormal termination.\n", cerr); }
 // This function is for error messages.
 //    It takes a format argument priored by any number of data arguments.
 //    It formats and prints an error message, then exits.
-void fatal(const string &msg)
-{
+void
+fatal (const string& msg) {
     cout << flush;
     cerr << msg;
     cerr << "\nError exit\n";
@@ -70,13 +67,13 @@ void fatal(const string &msg)
 // Store the current date and time in the arguments.
 //      System's date format is: "Fri Jun  9 10:15:55 1995\n"
 //      After extraction, date is: "Fri Jun  9 1995"    hour is: "10:15:55"
-void when(string &date, string &hour)
-{
-    time_t now = time(nullptr);           // Get encoded date, time.
-    string nowstring = ctime(&now);       // Convert to string form.
-    date = nowstring.substr(0, 10);       // Extract day, month, date.
-    date.append(nowstring.substr(19, 5)); // Extract space and year.
-    hour = nowstring.substr(11, 8);       // Copy hour:minutes:seconds.
+void
+when( string& date, string& hour ) {
+    time_t now = time(nullptr);                // Get encoded date, time.
+    string nowstring = ctime(&now);            // Convert to string form.
+    date = nowstring.substr(0, 10); // Extract day, month, date.
+    date.append(nowstring.substr(19, 5));      // Extract space and year.
+    hour = nowstring.substr(11, 8); // Copy hour:minutes:seconds.
 }
 
 // -------------------------------------------------------------------
@@ -84,12 +81,11 @@ void when(string &date, string &hour)
 //      date format is: "Fri Jun  9 1995"
 // -------------------------------------------------------------------
 string
-today()
-{
-    time_t now = time(nullptr);            // Get encoded date, time.
-    string nowstring = ctime(&now);        // Convert to string form.
-    string date = nowstring.substr(0, 10); // Copy hour:minutes:seconds.
-    date.append(nowstring.substr(19, 5));  // Copy hour:minutes:seconds.
+today() {
+    time_t now = time(nullptr);                // Get encoded date, time.
+    string nowstring = ctime(&now);            // Convert to string form.
+    string date = nowstring.substr(0, 10);     // Copy hour:minutes:seconds.
+    date.append(nowstring.substr(19, 5));      // Copy hour:minutes:seconds.
     return date;
 }
 
@@ -98,11 +94,10 @@ today()
 //      hour format is: "10:15:55"
 // -------------------------------------------------------------------
 string
-oclock()
-{
-    time_t now = time(nullptr);            // Get encoded date, time.
-    string nowstring = ctime(&now);        // Convert to string form.
-    string hour = nowstring.substr(11, 8); // Extract hour, minutes, seconds.
+oclock() {
+    time_t now = time(nullptr);                // Get encoded date, time.
+    string nowstring = ctime(&now);            // Convert to string form.
+    string hour = nowstring.substr(11, 8);     // Extract hour, minutes, seconds.
     return hour;
 }
 
@@ -110,57 +105,48 @@ oclock()
 //  Menu handling
 // ---------------------------------------------------------------------
 // Display a menu then read an alphabetic menu choice character.
-char menu_c(string title, int n, const string menu[], const string valid)
-{
+char menu_c( string title, int n, const string menu[], const string valid ){
     int k;
     char choice;
 
-    for (;;)
-    {
-        Printer::write("\n" + title + "\n", cout);
-        for (k = 0; k < n; ++k)
-            Printer::write("\t " + menu[k] + "\n", cout);
-        Printer::write("\n Enter code of desired item: ", cout);
+    for(;;) {
+        Printer::write("\n" +title +"\n",cout);
+        for( k=0; k<n; ++k ) Printer::write("\t "+menu[k]+"\n",cout);
+        Printer::write("\n Enter code of desired item: ",cout);
         cin >> choice;
-        if (valid.find(static_cast<char>(tolower(choice))) != string::npos)
-            break;
-        Printer::write("Illegal entry, try again.\n", cout);
+        if (valid.find(static_cast<char>(tolower(choice)))!=string::npos) break;
+        Printer::write("Illegal entry, try again.\n",cout);
     }
     choice = static_cast<char>(tolower(choice));
     string st;
-    st += choice;
-    Printer::write(st + "\n", cout);
+    st+=choice;
+    Printer::write(st+"\n",cout);
     return choice;
 }
 
 // ----------------------------------------------------------------------------
 // Display a menu then read and validate a numeric menu choice.
-int menu(const char *title, int n, const string menu[])
-{
+int menu( const char* title, int n, const string menu[] ){
     int choice;
 
-    cout << '\n'
-         << title << '\n'; // she wanted this
-    for (;;)
-    {
-        for (int k = 0; k < n; ++k)
-            cout << "\t " << (k + 1) << ". " << menu[k] << '\n';
+    cout << '\n' << title << '\n';//she wanted this
+    for(;;) {
+        for( int k=0; k<n; ++k )
+            cout << "\t " << (k+1) << ". " << menu[k] <<'\n';
 
         cout << "\n Enter number of desired item: ";
         cin >> choice >> flush;
-        if (0 < choice && choice <= n)
-            break;
+        if ( 0 < choice && choice <= n) break;
         cout << "\n Illegal choice or input error; try again.";
     }
     return choice;
 }
 
+
 // ----------------------------------------------------------------------------
-// case Insensitivity Checker
-bool caseInsensitiveEquals(const string &str1, const string &str2)
-{
-    if (str1.length() != str2.length())
-    {
+//case Insensitivity Checker
+bool caseInsensitiveEquals(const string& str1, const string& str2) {
+    if (str1.length() != str2.length()) {
         return false;
     }
     string lowerStr1 = str1;
@@ -169,3 +155,4 @@ bool caseInsensitiveEquals(const string &str1, const string &str2)
     transform(lowerStr2.begin(), lowerStr2.end(), lowerStr2.begin(), ::tolower);
     return lowerStr1 == lowerStr2;
 }
+
